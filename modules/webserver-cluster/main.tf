ac09ed1124c6c8720db5b5ee18ed50b2803d96d7
RuariW12 <ruariw16@gmail.com>
2025-11-21 13:33:05 -0500

    Fixed main.tf in stage

--- modules/webserver-cluster/main.tf
+++ modules/webserver-cluster/main.tf
@@ -13,3 +13,34 @@ data "aws_subnets" "default" {
     }
 }
 
+#ECR Repository
+
+resource "aws_ecr_repository" "divot_backend" {
+  name = var.ecr_repo_name
+
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+#IAM role for EC2
+resource "aws_iam_role" "ec2_role" {
+  name = "divot-ec2-role-${var.environment}"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+
